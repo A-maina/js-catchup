eccbe9c5bef4face0d2c0d63a9988fa1d4cd8af5
allan <4lwachira@gmail.com>
2024-07-04 17:10:30 +0300

    js

--- script/app.js
+++ script/app.js
@@ -1,34 +1,68 @@
-const menuLinks = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
+const menuLinks = ["Home", "About", "Contact", "Blog", "Careers"];
 
 const socialMediaLinks = [
-	{
-		linkName: 'Facebook',
-		linkUrl: 'https://facebook.com',
-		icon: `<i class="fa-brands fa-facebook"></i>`,
-	},
-	{
-		linkName: 'Twitter',
-		linkUrl: 'https://twitter.com',
-		icon: `<i class="fa-brands fa-twitter"></i>`,
-	},
-	{
-		linkName: 'Instagram',
-		linkUrl: 'https://instagram.com',
-		icon: `<i class="fa-brands fa-instagram"></i>`,
-	},
-	{
-		linkName: 'GitHub',
-		linkUrl: 'https://github.com',
-		icon: `<i class="fa-brands fa-github"></i>`,
-	},
-	{
-		linkName: 'LinkedIn',
-		linkUrl: 'https://linkedin.com',
-		icon: `<i class="fa-brands fa-linkedin"></i>`,
-	},
-	{
-		linkName: 'Dribbble',
-		linkUrl: 'https://dribbble.com',
-		icon: `<i class="fa-brands fa-dribbble"></i>`,
-	},
+  {
+    linkName: "Facebook",
+    linkUrl: "https://facebook.com",
+    icon: `<i class="fa-brands fa-facebook"></i>`,
+  },
+  {
+    linkName: "Twitter",
+    linkUrl: "https://twitter.com",
+    icon: `<i class="fa-brands fa-twitter"></i>`,
+  },
+  {
+    linkName: "Instagram",
+    linkUrl: "https://instagram.com",
+    icon: `<i class="fa-brands fa-instagram"></i>`,
+  },
+  {
+    linkName: "GitHub",
+    linkUrl: "https://github.com",
+    icon: `<i class="fa-brands fa-github"></i>`,
+  },
+  {
+    linkName: "LinkedIn",
+    linkUrl: "https://linkedin.com",
+    icon: `<i class="fa-brands fa-linkedin"></i>`,
+  },
+  {
+    linkName: "Dribbble",
+    linkUrl: "https://dribbble.com",
+    icon: `<i class="fa-brands fa-dribbble"></i>`,
+  },
 ];
+const navBar = document.querySelector("nav");
+const result = menuLinks.map((link) => {
+  const navlink = document.createElement("a");
+
+  navlink.textContent = link;
+  navlink.href = "#";
+
+  navBar.appendChild(navlink);
+});
+const socialMediaContainer = document.querySelector(".social-media-links");
+
+socialMediaLinks.forEach((socialMediaLink) => {
+  const link = document.createElement("a");
+
+  link.href = socialMediaLink.linkUrl;
+
+  link.innerHTML = socialMediaLink.icon + " " + socialMediaLink.linkName;
+  link.className = "link"
+  socialMediaContainer.appendChild(link);
+});
+
+const mainHeading = document.createElement("h1");
+
+mainHeading.textContent = "This is a JS catchup Session";
+
+//const mainTag = document.querySelector("main");
+
+const sectionTag = document.createElement("section");
+
+sectionTag.appendChild(mainHeading);
+
+const body = document.querySelector("body");
+
+body.appendChild(sectionTag);
--- script/users.js
+++ script/users.js
@@ -1,44 +1,75 @@
 const users = [
-	{
-		name: {
-			firstName: 'Kamau',
-			lastName: 'Muchangi',
-		},
-		age: 30,
-		occupation: 'Software Engineer',
-		hobbies: ['reading', 'hiking', 'coding'],
-		image: 'https://placeholder.com/100x100',
-	},
-	{
-		name: {
-			firstName: 'William',
-			lastName: 'Omondi',
-		},
-		age: 25,
-		occupation: 'Data Scientist',
-		hobbies: ['swimming', 'traveling', 'cooking'],
-		image: 'https://placeholder.com/100x100',
-	},
+  {
+    name: {
+      firstName: "Kamau",
+      lastName: "Muchangi",
+    },
+    age: 30,
+    occupation: "Software Engineer",
+    hobbies: ["reading", "hiking", "coding"],
+    image: "https://placeholder.com/100x100",
+  },
+  {
+    name: {
+      firstName: "William",
+      lastName: "Omondi",
+    },
+    age: 25,
+    occupation: "Data Scientist",
+    hobbies: ["swimming", "traveling", "cooking"],
+    image: "https://placeholder.com/100x100",
+  },
 
-	{
-		name: {
-			firstName: 'Kabi',
-			lastName: 'Wa Judas',
-		},
-		age: 35,
-		occupation: 'Manager',
-		hobbies: ['fishing', 'hiking', 'coding'],
-		image: 'https://placeholder.com/100x100',
-	},
+  {
+    name: {
+      firstName: "Kabi",
+      lastName: "Wa Judas",
+    },
+    age: 35,
+    occupation: "Manager",
+    hobbies: ["fishing", "hiking", "coding"],
+    image: "https://placeholder.com/100x100",
+  },
 
-	{
-		name: {
-			firstName: 'Alice',
-			lastName: 'Makori',
-		},
-		age: 40,
-		occupation: 'Designer',
-		hobbies: ['reading', 'hiking', 'painting'],
-		image: 'https://placeholder.com/100x100',
-	},
+  {
+    name: {
+      firstName: "Alice",
+      lastName: "Makori",
+    },
+    age: 40,
+    occupation: "Designer",
+    hobbies: ["reading", "hiking", "painting"],
+    image: "https://placeholder.com/100x100",
+  },
 ];
+const mainTag = document.querySelector("main");
+users.map((user) => {
+  const userCard = document.createElement("div");
+  userCard.className = "user-card";
+
+  const image = document.createElement("img");
+  image.src = user.image;
+
+  const firstName = document.createElement("h3");
+  firstName.textContent = user.name.firstName;
+
+  const secondName = document.createElement("h3");
+  secondName.textContent = user.name.lastName;
+
+  const hobbies = document.createElement("p");
+  hobbies.textContent = " Hobbies " + user.hobbies.join(",");
+hobbies.className ="hobbies"
+  const age = document.createElement("p");
+  age.textContent = user.age;
+
+  const occupation = document.createElement("p");
+  occupation.textContent = user.occupation;
+
+  userCard.appendChild(image);
+  userCard.appendChild(firstName);
+  userCard.appendChild(secondName);
+  userCard.appendChild(hobbies);
+  userCard.appendChild(age);
+  userCard.appendChild(occupation);
+  mainTag.appendChild(userCard);
+});
